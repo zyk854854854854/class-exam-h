e6643a9226878cc2609bb4ad7ab3042794203d37
tjk1898 <tjk1898@gmail.com>
2023-04-27 17:11:37 +0800

    将项目恢复到只完成"员工管理"部分。 此时，项目可以完成的功能： 1. 登录、登出 2. 通过filter进行的登录权限认证 3. 跨域访问 4. 自动填充更新时间，更新用户，创建时间，创建用户 5. 使用R统一返回格式

--- src/main/java/edu/czjt/reggie/mapper/DishFlavorMapper.java
+++
@@ -1,9 +0,0 @@
-package edu.czjt.reggie.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import edu.czjt.reggie.entity.DishFlavor;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DishFlavorMapper extends BaseMapper<DishFlavor> {
-}
--- src/main/java/edu/czjt/reggie/service/DishFlavorService.java
+++
@@ -1,7 +0,0 @@
-package edu.czjt.reggie.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import edu.czjt.reggie.entity.DishFlavor;
-
-public interface DishFlavorService extends IService<DishFlavor> {
-}
