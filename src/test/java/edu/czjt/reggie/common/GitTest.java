4ed9fc9d18034afaa2ce823507caed9e2085631b
tjk1898 <tjk1898@gmail.com>
2023-05-24 08:17:58 +0800

    Git操作演示,添加一个方法

--- src/test/java/edu/czjt/reggie/common/GitTest.java
+++ src/test/java/edu/czjt/reggie/common/GitTest.java
@@ -4,4 +4,7 @@ package edu.czjt.reggie.common;
  * Created by jinkun.tian on 2023/5/24
  */
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hehe");
+    }
 }
